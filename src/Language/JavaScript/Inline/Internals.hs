4af144fb797530c55b77aa7286faaaf5d6085de3
Shao Cheng <astrohavoc@gmail.com>
2018-03-10 15:02:47 +0800

    Implement MsgId, prep for moving to websockets

---
+++ src/Language/JavaScript/Inline/Internals.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE UnboxedTuples #-}
+
+module Language.JavaScript.Inline.Internals
+  ( MsgId(..)
+  , newMsgId
+  ) where
+
+import Data.Aeson
+import Data.Hashable
+import Data.Primitive.ByteArray
+import Data.Primitive.Types
+import GHC.Exts
+import GHC.Types
+import System.IO.Unsafe
+
+newtype MsgId = MsgId
+  { unMsgId :: Int
+  } deriving (Show, Hashable, FromJSON, ToJSON)
+
+{-# NOINLINE msgIdRef #-}
+msgIdRef :: MutableByteArray RealWorld
+msgIdRef =
+  unsafePerformIO $ do
+    mba <- newByteArray $ sizeOf (0 :: Int)
+    writeByteArray mba 0 (0 :: Int)
+    pure mba
+
+{-# INLINEABLE newMsgId #-}
+newMsgId :: IO MsgId
+newMsgId =
+  IO
+    (\s0 ->
+       case fetchAddIntArray# mba 0# 1# s0 of
+         (# s1, x #) -> (# s1, MsgId (I# x) #))
+  where
+    !(MutableByteArray mba) = msgIdRef
