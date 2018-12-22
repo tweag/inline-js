1a8cfc4b5b231d906fbf930ac8793357259e6960
Sebastian Kazenbroot-Guppy <sebastian.kg@gmail.com>
2018-12-21 22:34:24 -0800

    Using tasty for testing (#4)
    * Added Tweag style guide compiler flags
    
    * newtype-wrapped the JSCode type
    
    * Un-aliased JSON types for Objects, Arrays and Strings
    
    * Used tasty (with HSpec and Quickcheck) to upgrade existing tests
    
    * Fixed indenting on ghc-options
    
    * Updated the CircleCI test command
    
    * Removed duplicate tests entry

---
+++ inline-js/tests/Tests/Evaluation.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NamedFieldPuns #-}
+
+module Tests.Evaluation
+  ( tests
+  ) where
+
+import Control.Monad (void)
+import Language.JavaScript.Inline.JSON
+import Language.JavaScript.Inline.Message
+import Language.JavaScript.Inline.Session
+import Test.Tasty (TestTree)
+import Test.Tasty.Hspec (it, shouldBe, testSpec)
+import Tests.Helpers.Message
+
+tests :: IO TestTree
+tests =
+  testSpec "JSCode Evaluation" $
+  it "Should Handle Many Mixed-Async-and-Sync Requests" $
+  withJSSession defJSSessionOpts $ \s -> do
+    let requestTest (request, test) = do
+          msgId <- sendMsg s request
+          pure (msgId, test)
+        recvAndRunTest (msgId, test) = do
+          result <- recvMsg s msgId
+          test result
+    testPairs <-
+      traverse
+        requestTest
+        [ (syncEvaluation "while(true){}" `withEvalTimeout` 1000, failsToReturn)
+        , (syncEvaluation "BOOM", failsToReturn)
+        , (syncEvaluation "undefined", successfullyReturns Null)
+        , (syncEvaluation "let x = 6*7", successfullyReturns Null)
+        , (syncEvaluation "x", successfullyReturns $ Number 42)
+        , ( syncEvaluation "\"left\" + \"pad\""
+          , successfullyReturns $ String "leftpad")
+        , (asyncEvaluation "Promise.reject('BOOM')", failsToReturn)
+        , ( asyncEvaluation "Promise.resolve(x)"
+          , successfullyReturns $ Number 42)
+        , ( asyncEvaluation
+              "new Promise((resolve, _) => setTimeout(resolve, 10000))" `withResolveTimeout`
+            1000
+          , failsToReturn)
+        ]
+    void $ traverse recvAndRunTest testPairs
+
+failsToReturn :: RecvMsg -> IO ()
+failsToReturn Result {isError} = isError `shouldBe` True
+
+successfullyReturns :: Value -> RecvMsg -> IO ()
+successfullyReturns expected Result {isError, result} = do
+  isError `shouldBe` False
+  result `shouldBe` expected
