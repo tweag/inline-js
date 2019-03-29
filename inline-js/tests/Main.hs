a74eeacde4488adf6a82898c2f752ab5161ec7f1
Shao Cheng <cheng.shao@tweag.io>
2019-03-29 21:45:11 +0800

    Merge test suite of inline-js-core and inline-js

--- inline-js/tests/Main.hs
+++ inline-js/tests/Main.hs
@@ -1,7 +1,9 @@
 import Test.Tasty (defaultMain, testGroup)
+import qualified Tests.Evaluation as Evaluation
+import qualified Tests.PingPong as PingPong
 import qualified Tests.Quotation as Quotation
 
 main :: IO ()
 main = do
-  tests <- sequence [Quotation.tests]
+  tests <- sequence [Evaluation.tests, PingPong.tests, Quotation.tests]
   defaultMain $ testGroup "inline-js Test Suite" tests
