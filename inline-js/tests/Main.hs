48448d6ed720beb6a05a84589cdae9f717abf757
Shao Cheng <cheng.shao@tweag.io>
2019-04-10 17:39:34 +0800

    Implement Wasm test

--- inline-js/tests/Main.hs
+++ inline-js/tests/Main.hs
@@ -2,8 +2,10 @@ import Test.Tasty (defaultMain, testGroup)
 import qualified Tests.Evaluation as Evaluation
 import qualified Tests.PingPong as PingPong
 import qualified Tests.Quotation as Quotation
+import qualified Tests.Wasm as Wasm
 
 main :: IO ()
 main = do
-  tests <- sequence [Evaluation.tests, PingPong.tests, Quotation.tests]
+  tests <-
+    sequence [Evaluation.tests, PingPong.tests, Quotation.tests, Wasm.tests]
   defaultMain $ testGroup "inline-js Test Suite" tests
