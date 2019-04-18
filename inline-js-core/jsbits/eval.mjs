187e897cfd82bccede9c53b02ce66c1d0533d7a4
Shao Cheng <cheng.shao@tweag.io>
2019-04-18 23:26:17 +0800

    Renaissance

--- inline-js-core/jsbits/eval.mjs
+++ inline-js-core/jsbits/eval.mjs
@@ -1,100 +1,50 @@
-import fs from "fs";
+import path from "path";
 import process from "process";
 import { StringDecoder } from "string_decoder";
 import url from "url";
 import vm from "vm";
+import { Worker } from "worker_threads";
 
 import context_global from "./context.mjs";
-import { Transport } from "./transport.mjs";
 
 process.on("uncaughtException", err => {
   process.stderr.write(err.stack);
   throw err;
 });
 
-const ctx = vm.createContext(context_global);
-
-const ipc = new Transport(
-  fs.createReadStream(null, {
-    encoding: null,
-    fd: Number.parseInt(process.argv[process.argv.length - 1]),
-    autoClose: false
-  }),
-  fs.createWriteStream(null, {
-    encoding: null,
-    fd: Number.parseInt(process.argv[process.argv.length - 2]),
-    autoClose: false
-  })
-);
-
-function bufferFromU32(x) {
-  const buf = Buffer.allocUnsafe(4);
-  buf.writeUInt32LE(x, 0);
-  return buf;
-}
+const ctx = vm.createContext(context_global),
+  decoder = new StringDecoder("utf8"),
+  ipc = new Worker(
+    path.join(
+      path.dirname(url.fileURLToPath(import.meta.url)),
+      "transport.mjs"
+    ),
+    {
+      workerData: [
+        Number.parseInt(process.argv[process.argv.length - 1]),
+        Number.parseInt(process.argv[process.argv.length - 2])
+      ]
+    }
+  );
 
 function sendMsg(msg_id, ret_tag, is_err, result) {
-  try {
-    switch (ret_tag) {
-      case 0: {
-        const result_buf = Buffer.from(result),
-          msg_buf = Buffer.allocUnsafe(8 + result_buf.length);
-        msg_buf.writeUInt32LE(msg_id, 0);
-        msg_buf.writeUInt32LE(Number(is_err), 4);
-        result_buf.copy(msg_buf, 8);
-        ipc.send(msg_buf);
-        break;
-      }
-      case 1: {
-        const msg_buf = Buffer.allocUnsafe(12);
-        msg_buf.writeUInt32LE(msg_id, 0);
-        msg_buf.writeUInt32LE(0, 4);
-        msg_buf.writeUInt32LE(ctx.JSVal.newJSVal(result), 8);
-        ipc.send(msg_buf);
-        break;
-      }
-      case 2: {
-        const msg_buf = Buffer.allocUnsafe(8);
-        msg_buf.writeUInt32LE(msg_id, 0);
-        msg_buf.writeUInt32LE(0, 4);
-        ipc.send(msg_buf);
-        break;
-      }
-      case 3: {
-        const [hs_func_ref, args] = result,
-          buf_args = args.flatMap(arg => {
-            const raw_buf = Buffer.from(arg);
-            return [bufferFromU32(raw_buf.length), raw_buf];
-          });
-        buf_args.unshift(
-          bufferFromU32(msg_id),
-          bufferFromU32(hs_func_ref),
-          bufferFromU32(args.length)
-        );
-        ipc.send(Buffer.concat(buf_args));
-        break;
-      }
-      default: {
-        throw new Error(`Unsupported ret_tag ${ret_tag}`);
-      }
-    }
-  } catch (err) {
-    sendMsg(msg_id, 0, true, err.toString());
-  }
+  ipc.postMessage([
+    msg_id,
+    ret_tag,
+    is_err,
+    ret_tag === 1 ? ctx.JSVal.newJSVal(result) : result
+  ]);
 }
 
-const decoder = new StringDecoder("utf8");
-
-ipc.on("recv", async buf => {
-  const msg_id = buf.readUInt32LE(0);
+ipc.on("message", async ([msg_id, msg_tag, buf]) => {
   try {
-    const msg_tag = buf.readUInt32LE(4);
+    buf = Buffer.from(buf);
     switch (msg_tag) {
       case 0: {
-        const ret_tag = buf.readUInt32LE(8),
-          eval_timeout = buf.readUInt32LE(12),
-          resolve_timeout = buf.readUInt32LE(16),
-          msg_content = decoder.end(buf.slice(20)),
+        const ret_tag = buf.readUInt32LE(0),
+          eval_timeout = buf.readUInt32LE(4),
+          resolve_timeout = buf.readUInt32LE(8),
+          msg_content = decoder.end(buf.slice(12)),
           eval_options = {
             displayErrors: true,
             importModuleDynamically: spec => import(spec)
@@ -117,18 +67,18 @@ ipc.on("recv", async buf => {
         break;
       }
       case 1: {
-        sendMsg(msg_id, 1, false, buf.slice(8));
+        sendMsg(msg_id, 1, false, buf);
         break;
       }
       case 2: {
-        const import_path = decoder.end(buf.slice(8)),
+        const import_path = decoder.end(buf),
           import_url = url.pathToFileURL(import_path).href,
           import_result = await import(import_url);
         sendMsg(msg_id, 1, false, import_result);
         break;
       }
       case 3: {
-        const hs_func_ref = buf.readUInt32LE(8);
+        const hs_func_ref = buf.readUInt32LE(0);
         sendMsg(
           msg_id,
           1,
@@ -143,8 +93,8 @@ ipc.on("recv", async buf => {
       }
       case 4: {
         const [resolve, reject] = ctx.JSVal.takeJSVal(msg_id),
-          is_err = Boolean(buf.readUInt32LE(8)),
-          hs_result = buf.slice(12);
+          is_err = Boolean(buf.readUInt32LE(0)),
+          hs_result = buf.slice(4);
         (is_err ? reject : resolve)(hs_result);
         break;
       }
--- inline-js-core/jsbits/transport.mjs
+++ inline-js-core/jsbits/transport.mjs
@@ -1,10 +1,19 @@
-import assert from "assert";
-import { EventEmitter } from "events";
+import fs from "fs";
+import { parentPort, workerData } from "worker_threads";
 
-export class Transport extends EventEmitter {
+function bufferFromU32(x) {
+  const buf = Buffer.allocUnsafe(4);
+  buf.writeUInt32LE(x, 0);
+  return buf;
+}
+
+class Transport {
   constructor(i, o) {
-    super();
-    this.i = i;
+    this.i = fs.createReadStream(null, {
+      encoding: null,
+      fd: i,
+      autoClose: false
+    });
     this.o = o;
     this.iMsgLen = 0;
     this.iRest = Buffer.allocUnsafe(0);
@@ -15,30 +24,67 @@ export class Transport extends EventEmitter {
         if (!this.iMsgLen) {
           if (this.iRest.length < 4) break;
           this.iMsgLen = this.iRest.readUInt32LE(0);
-          assert(this.iMsgLen > 0);
           this.iRest = this.iRest.slice(4);
         }
         if (this.iRest.length < this.iMsgLen) break;
-        this.emit("recv", this.iRest.slice(0, this.iMsgLen));
+        const msg_buf = this.iRest.slice(0, this.iMsgLen),
+          msg_id = msg_buf.readUInt32LE(0),
+          msg_tag = msg_buf.readUInt32LE(4);
+        parentPort.postMessage([msg_id, msg_tag, msg_buf.slice(8)]);
         this.iRest = this.iRest.slice(this.iMsgLen);
         this.iMsgLen = 0;
       }
     });
-    this.i.on("error", err => this.emit("error", err));
-    this.o.on("error", err => this.emit("error", err));
-  }
-
-  send(buf) {
-    return new Promise((resolve, reject) => {
-      const nbuf = Buffer.allocUnsafe(buf.length + 4);
-      nbuf.writeUInt32LE(buf.length);
-      buf.copy(nbuf, 4);
-      this.o.write(nbuf, err => {
-        if (typeof err === "undefined") {
-          this.emit("send", buf);
-          resolve();
-        } else reject(err);
-      });
-    });
   }
 }
+
+const ipc = new Transport(workerData[0], workerData[1]);
+
+parentPort.on("message", ([msg_id, ret_tag, is_err, result]) => {
+  let buf;
+  switch (ret_tag) {
+    case 0: {
+      const result_buf = Buffer.from(result),
+        msg_buf = Buffer.allocUnsafe(8 + result_buf.length);
+      msg_buf.writeUInt32LE(msg_id, 0);
+      msg_buf.writeUInt32LE(Number(is_err), 4);
+      result_buf.copy(msg_buf, 8);
+      buf = msg_buf;
+      break;
+    }
+    case 1: {
+      const msg_buf = Buffer.allocUnsafe(12);
+      msg_buf.writeUInt32LE(msg_id, 0);
+      msg_buf.writeUInt32LE(0, 4);
+      msg_buf.writeUInt32LE(result, 8);
+      buf = msg_buf;
+      break;
+    }
+    case 2: {
+      const msg_buf = Buffer.allocUnsafe(8);
+      msg_buf.writeUInt32LE(msg_id, 0);
+      msg_buf.writeUInt32LE(0, 4);
+      buf = msg_buf;
+      break;
+    }
+    case 3: {
+      const [hs_func_ref, args] = result,
+        buf_args = args.flatMap(arg => {
+          const raw_buf = Buffer.from(arg);
+          return [bufferFromU32(raw_buf.length), raw_buf];
+        });
+      buf_args.unshift(
+        bufferFromU32(msg_id),
+        bufferFromU32(hs_func_ref),
+        bufferFromU32(args.length)
+      );
+      buf = Buffer.concat(buf_args);
+      break;
+    }
+    default: {
+      throw new Error(`Unsupported ret_tag ${ret_tag}`);
+    }
+  }
+  fs.writeSync(ipc.o, bufferFromU32(buf.length));
+  fs.writeSync(ipc.o, buf);
+});
