3f9fd380df500d59ea9140d24d155b93077294dc
Shao Cheng <cheng.shao@tweag.io>
2019-10-27 22:36:09 +0800

    Decouple request & response type (#34)

--- inline-js-core/src/Language/JavaScript/Inline/Core/Message/HSCode.hs
+++ inline-js-core/src/Language/JavaScript/Inline/Core/Message/HSCode.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE StrictData #-}
-{-# LANGUAGE TypeFamilies #-}
 
 module Language.JavaScript.Inline.Core.Message.HSCode
   ( ExportHSFuncRequest (..),
@@ -9,9 +8,7 @@ where
 
 import Data.Binary.Put
 import Language.JavaScript.Inline.Core.HSCode
-import Language.JavaScript.Inline.Core.JSCode
 import Language.JavaScript.Inline.Core.Message.Class
-import Language.JavaScript.Inline.Core.Message.Eval
 
 data ExportHSFuncRequest
   = ExportHSFuncRequest
@@ -20,9 +17,6 @@ data ExportHSFuncRequest
       }
 
 instance Request ExportHSFuncRequest where
-
-  type ResponseOf ExportHSFuncRequest = EvalResponse JSVal
-
   putRequest ExportHSFuncRequest {exportHSFuncRef = HSFuncRef r, ..} = do
     putWord32host $ if sync then 5 else 3
     putWord32host $ fromIntegral r
