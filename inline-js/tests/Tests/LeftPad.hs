c9463525dc8aa33cbee99ea5b40a22c58adb63c4
Shao Cheng <cheng.shao@tweag.io>
2019-11-21 11:12:39 +0100

    Allow the test suite to run without internet (#49)

--- inline-js/tests/Tests/LeftPad.hs
+++ inline-js/tests/Tests/LeftPad.hs
@@ -9,31 +9,39 @@ import qualified Data.ByteString.Lazy as LBS
 import Distribution.Simple.Utils
 import Distribution.Verbosity
 import Language.JavaScript.Inline.Core
+import qualified Paths_inline_js
 import System.Directory
+import System.FilePath
 import System.Process
 import Test.Tasty
 import Test.Tasty.HUnit
 
 tests :: IO TestTree
-tests =
+tests = do
+  datadir <- Paths_inline_js.getDataDir
+  tmpdir <- getTemporaryDirectory
   pure $
     testGroup
       "left-pad"
       [ testCase "should install left-pad from npm and run it" $ do
-          tmpdir <- getTemporaryDirectory
           x <-
-            withTempDirectory silent tmpdir "inline-js-test-suite-left-pad" $ \p ->
-              do
+            withTempDirectory silent tmpdir "inline-js-test-suite-left-pad" $
+              \p -> do
                 _ <-
                   readCreateProcess
-                    ( (shell "npm install left-pad")
+                    ( ( shell
+                          ( "npm install "
+                              <> (datadir </> "testdata" </> "left-pad-1.3.0.tar")
+                          )
+                      )
                         { cwd = Just p,
                           std_err = CreatePipe
                         }
                     )
                     ""
-                withJSSession defJSSessionOpts {nodeWorkDir = Just p} $ \s -> do
-                  mref <- eval s "import('left-pad').then(m => m.default)"
-                  eval s $ deRefJSVal mref <> "('foo', 5)"
+                withJSSession defJSSessionOpts {nodeWorkDir = Just p} $ \s ->
+                  do
+                    mref <- eval s "import('left-pad').then(m => m.default)"
+                    eval s $ deRefJSVal mref <> "('foo', 5)"
           x @?= ("  foo" :: LBS.ByteString)
       ]
