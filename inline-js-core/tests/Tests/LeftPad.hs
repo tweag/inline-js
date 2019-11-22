d2f6423ac4ae8d463d31652ff8e39af0f7407e45
Shao Cheng <cheng.shao@tweag.io>
2019-11-22 17:49:13 +0100

    Remove dependency on npm for left-pad test

--- inline-js-core/tests/Tests/LeftPad.hs
+++ inline-js-core/tests/Tests/LeftPad.hs
@@ -23,25 +23,13 @@ tests = do
   pure $
     testGroup
       "left-pad"
-      [ testCase "should install left-pad from npm and run it" $ do
-          x <-
-            withTempDirectory silent tmpdir "inline-js-test-suite-left-pad" $
-              \p -> do
-                _ <-
-                  readCreateProcess
-                    ( ( shell
-                          ( "npm install "
-                              <> (datadir </> "testdata" </> "left-pad-1.3.0.tar")
-                          )
-                      )
-                        { cwd = Just p,
-                          std_err = CreatePipe
-                        }
-                    )
-                    ""
-                withJSSession defJSSessionOpts {nodeWorkDir = Just p} $ \s ->
-                  do
-                    mref <- eval s "import('left-pad').then(m => m.default)"
-                    eval s $ deRefJSVal mref <> "('foo', 5)"
+      [ testCase "should run left-pad" $ do
+          x <- withTempDirectory silent tmpdir "left-pad" $ \p -> do
+            callProcess
+              "tar"
+              ["-C", p, "-x", "-f", datadir </> "testdata" </> "left-pad.tar"]
+            withJSSession defJSSessionOpts {nodeWorkDir = Just p} $ \s -> do
+              mref <- eval s "import('left-pad').then(m => m.default)"
+              eval s $ deRefJSVal mref <> "('foo', 5)"
           x @?= ("  foo" :: LBS.ByteString)
       ]
