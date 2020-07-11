ba4048fa0cd5d27c7bf2b1ee418cee38f7665aee
Shao Cheng <cheng.shao@tweag.io>
2020-07-11 22:21:30 +0200

    Export class

---
+++ inline-js-core/src/Language/JavaScript/Inline/Core/Export.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Language.JavaScript.Inline.Core.Export where
+
+import qualified Data.ByteString.Lazy as LBS
+import Data.Proxy
+import Language.JavaScript.Inline.Core.Class
+import Language.JavaScript.Inline.Core.Message
+import Language.JavaScript.Inline.Core.Session
+
+class Export f where
+  argsToRawJSType :: Proxy f -> [JSExpr]
+  export :: Session -> f -> [LBS.ByteString] -> IO JSExpr
+
+instance ToJS r => Export (IO r) where
+  argsToRawJSType _ = []
+  export _ m [] = toJS <$> m
+  export _ _ _ = fail "Language.JavaScript.Inline.Core.Export: impossible"
+
+instance (FromJS a, Export b) => Export (a -> b) where
+  argsToRawJSType _ = toRawJSType (Proxy @a) : argsToRawJSType (Proxy @b)
+  export s f (x : xs) = do
+    a <- fromRawJSType =<< rawFromJS s x
+    export @b s (f a) xs
+  export _ _ _ = fail "Language.JavaScript.Inline.Core.Export: impossible"
