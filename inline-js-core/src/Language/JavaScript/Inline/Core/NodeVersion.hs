df0fde5b87756d7ac66e0c0c0abff65fece4806d
Shao Cheng <cheng.shao@tweag.io>
2020-07-13 13:16:49 +0200

    Simplify parsing of node version

--- inline-js-core/src/Language/JavaScript/Inline/Core/NodeVersion.hs
+++ inline-js-core/src/Language/JavaScript/Inline/Core/NodeVersion.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Language.JavaScript.Inline.Core.NodeVersion where
 
 import Control.Exception
@@ -13,18 +15,20 @@ parseNodeVersion s0 = Version vs $ case tag of
   "" -> []
   _ -> [tag]
   where
-    vs_ts = split (== '-') $ tail s0
+    vs_ts = split (== '-') s0
     vs = map read $ split (== '.') $ head vs_ts
     tag = intercalate "-" $ tail vs_ts
 
 isSupportedVersion :: Version -> Bool
-isSupportedVersion v =
-  (v >= makeVersion [12, 0, 0])
-    || (v >= makeVersion [10, 20, 0] && v < makeVersion [11])
+isSupportedVersion (versionBranch -> v) =
+  (v >= [12, 0, 0]) || (v >= [10, 20, 0] && v < [11])
 
 checkNodeVersion :: FilePath -> IO ()
 checkNodeVersion p = do
   v <-
     parseNodeVersion
-      <$> readProcess p ["--eval", "process.stdout.write(process.version)"] ""
+      <$> readProcess
+        p
+        ["--eval", "process.stdout.write(process.versions.node)"]
+        ""
   unless (isSupportedVersion v) $ throwIO NodeVersionUnsupported
