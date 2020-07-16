d3805ccbfae82e68eb952d7b1bd24645d2e93da4
Cheng Shao <cheng.shao@tweag.io>
2020-07-16 14:16:35 +0200

    WIP

--- inline-js-examples/src/Language/JavaScript/Inline/Examples/Wasm.hs
+++ inline-js-examples/src/Language/JavaScript/Inline/Examples/Wasm.hs
@@ -6,6 +6,7 @@
 module Language.JavaScript.Inline.Examples.Wasm where
 
 import Control.Exception
+import qualified Data.ByteString.Lazy as LBS
 import Language.JavaScript.Inline
 
 newtype F64 = F64 Double
@@ -28,3 +29,11 @@ importAdd _session _import_obj (Aeson -> _import_module) (Aeson -> _import_name)
           }
           $_import_obj[$_import_module][$_import_name] = $_import_js_func;
         |]
+
+wasmCompile :: Session -> LBS.ByteString -> IO JSVal
+wasmCompile _session _module_buf =
+  eval _session [expr| WebAssembly.compile($_module_buf) |]
+
+wasmInstantiate :: Session -> JSVal -> JSVal -> IO JSVal
+wasmInstantiate _session _module _import_obj =
+  eval _session [expr| WebAssembly.instantiate($_module, $_import_obj) |]
