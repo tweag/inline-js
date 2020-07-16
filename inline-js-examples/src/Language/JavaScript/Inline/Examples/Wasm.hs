3a37ade14759b5cfa4a562e99a131386ff114032
Cheng Shao <cheng.shao@tweag.io>
2020-07-16 18:56:15 +0200

    WIP

--- inline-js-examples/src/Language/JavaScript/Inline/Examples/Wasm.hs
+++ inline-js-examples/src/Language/JavaScript/Inline/Examples/Wasm.hs
@@ -7,11 +7,57 @@ module Language.JavaScript.Inline.Examples.Wasm where
 
 import Control.Exception
 import qualified Data.ByteString.Lazy as LBS
+import Foreign
 import Language.JavaScript.Inline
+import Language.JavaScript.Inline.Examples.Utils
+
+newtype I32 = I32 Word32
+  deriving (Show, Storable) via Word32
+
+newtype I64 = I64 Word64
+  deriving (Show, Storable) via Word64
+
+newtype F32 = F32 Float
+  deriving (Show, Storable) via Float
 
 newtype F64 = F64 Double
-  deriving (Show) via Double
-  deriving (ToJS, FromJS) via (Aeson Double)
+  deriving (Show, Storable) via Double
+
+instance ToJS I32 where
+  toJS x = [expr| $buf.readUInt32LE() |] where buf = storableToLBS x
+
+instance FromJS I32 where
+  rawJSType _ = RawBuffer
+  toRawJSType _ =
+    [expr| x => { const buf = Buffer.allocUnsafe(4); buf.writeUInt32LE(x); return buf; } |]
+  fromJS _ = storableFromLBS
+
+instance ToJS I64 where
+  toJS x = [expr| $buf.readBigUInt64LE() |] where buf = storableToLBS x
+
+instance FromJS I64 where
+  rawJSType _ = RawBuffer
+  toRawJSType _ =
+    [expr| x => { const buf = Buffer.allocUnsafe(8); buf.writeBigUInt64LE(x); return buf; } |]
+  fromJS _ = storableFromLBS
+
+instance ToJS F32 where
+  toJS x = [expr| $buf.readFloatLE() |] where buf = storableToLBS x
+
+instance FromJS F32 where
+  rawJSType _ = RawBuffer
+  toRawJSType _ =
+    [expr| x => { const buf = Buffer.allocUnsafe(4); buf.writeFloatLE(x); return buf; } |]
+  fromJS _ = storableFromLBS
+
+instance ToJS F64 where
+  toJS x = [expr| $buf.readDoubleLE() |] where buf = storableToLBS x
+
+instance FromJS F64 where
+  rawJSType _ = RawBuffer
+  toRawJSType _ =
+    [expr| x => { const buf = Buffer.allocUnsafe(8); buf.writeDoubleLE(x); return buf; } |]
+  fromJS _ = storableFromLBS
 
 importNew :: Session -> IO JSVal
 importNew _session = eval _session [expr| {} |]
