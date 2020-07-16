488a148466fd5893175d64d9a8ea8ecafc5ef6fd
Cheng Shao <cheng.shao@tweag.io>
2020-07-16 13:55:18 +0200

    WIP

--- inline-js-examples/src/Language/JavaScript/Inline/Examples/Wasm.hs
+++ inline-js-examples/src/Language/JavaScript/Inline/Examples/Wasm.hs
@@ -1,1 +1,30 @@
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ViewPatterns #-}
+
 module Language.JavaScript.Inline.Examples.Wasm where
+
+import Control.Exception
+import Language.JavaScript.Inline
+
+newtype F64 = F64 Double
+  deriving (Show) via Double
+  deriving (ToJS, FromJS) via (Aeson Double)
+
+importNew :: Session -> IO JSVal
+importNew _session = eval _session [expr| {} |]
+
+importAdd :: Export f => Session -> JSVal -> String -> String -> f -> IO ()
+importAdd _session _import_obj (Aeson -> _import_module) (Aeson -> _import_name) _import_hs_func =
+  do
+    _import_js_func <- exportSync _session _import_hs_func
+    evaluate
+      =<< eval
+        _session
+        [block|
+          if (!($_import_obj[$_import_module])) {
+            $_import_obj[$_import_module] = {};
+          }
+          $_import_obj[$_import_module][$_import_name] = $_import_js_func;
+        |]
