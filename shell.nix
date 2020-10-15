4c42c0b29ee98c4ff07eaeffcf3875c9da97848c
Cheng Shao <cheng.shao@tweag.io>
2020-10-15 20:56:40 +0000

    WIP

---
+++ shell.nix
@@ -0,0 +1,30 @@
+{ sources ? import ./nix/sources.nix { }
+, haskellNix ? import (sources.haskell-nix) { }
+, nixpkgsSrc ? haskellNix.sources.nixpkgs-2009
+, nixpkgsArgs ? haskellNix.nixpkgsArgs
+, pkgs ? import nixpkgsSrc nixpkgsArgs
+, hsPkgs ? import ./default.nix { inherit pkgs; }
+}:
+  hsPkgs.shellFor {
+    packages = ps: with ps; [
+      inline-js
+      inline-js-core
+      inline-js-examples
+      inline-js-tests
+    ];
+
+    withHoogle = true;
+
+    buildInputs = with pkgs.haskellPackages;
+      [
+        brittany
+        cabal-install
+        ghcid
+        hlint
+        pkgs.nix
+        pkgs.nodejs-14_x
+        ormolu
+      ];
+
+    exactDeps = true;
+  }
