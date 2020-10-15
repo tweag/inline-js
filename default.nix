45d1cfd7e375413e4e9e058c235446c42623431d
Cheng Shao <cheng.shao@tweag.io>
2020-10-15 21:27:55 +0000

    WIP

--- default.nix
+++ default.nix
@@ -1,14 +1,15 @@
 { sources ? import ./nix/sources.nix { }
-, haskellNix ? import (sources.haskell-nix) { }
+, haskellNix ? import sources.haskell-nix { }
 , nixpkgsSrc ? haskellNix.sources.nixpkgs-2009
 , nixpkgsArgs ? haskellNix.nixpkgsArgs
 , pkgs ? import nixpkgsSrc nixpkgsArgs
+, ghc ? "ghc8102"
 }: pkgs.haskell-nix.cabalProject {
   src = pkgs.haskell-nix.haskellLib.cleanGit {
     name = "inline-js";
     src = ./.;
   };
-  compiler-nix-name = "ghc8102";
+  compiler-nix-name = ghc;
   modules = [{
     packages.inline-js-core.configureFlags = [
       ''--ghc-option=-DINLINE_JS_NODE=\"${pkgs.nodejs-14_x}/bin/node\"''
--- shell.nix
+++ shell.nix
@@ -1,8 +1,9 @@
 { sources ? import ./nix/sources.nix { }
-, haskellNix ? import (sources.haskell-nix) { }
+, haskellNix ? import sources.haskell-nix { }
 , nixpkgsSrc ? haskellNix.sources.nixpkgs-2009
 , nixpkgsArgs ? haskellNix.nixpkgsArgs
 , pkgs ? import nixpkgsSrc nixpkgsArgs
+, ghc ? "ghc8102"
 , hsPkgs ? import ./default.nix { inherit pkgs; }
 }: hsPkgs.shellFor {
   packages = ps: with ps; [
@@ -19,9 +20,9 @@
     cabal-install
     ghcid
     hlint
-    pkgs.nix
     pkgs.nodejs-14_x
     ormolu
+    (import sources.ghcide-nix {})."ghcide-${ghc}"
   ];
 
   exactDeps = true;
