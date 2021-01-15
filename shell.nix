003f785bc1646f6e6656f2960f7ade64a551fb36
Cheng Shao <cheng.shao@tweag.io>
2021-01-15 13:32:58 +0100

    WIP

--- shell.nix
+++ shell.nix
@@ -16,13 +16,32 @@
 
   withHoogle = true;
 
-  buildInputs = with pkgs.haskellPackages; [
-    brittany
-    cabal-install
-    ghcid
-    hlint
+  tools = {
+    brittany = "latest";
+    cabal-install = "latest";
+    ghcid = "latest";
+    hlint = "latest";
+    ormolu = "latest";
+  };
+
+  buildInputs = [
+    (pkgs.haskell-nix.cabalProject {
+      src = pkgs.fetchFromGitHub {
+        owner = "haskell";
+        repo = "haskell-language-server";
+        rev = "ghcide-v0.7.2";
+        sha256 = "12lal0q2dl4laknwix92i31gmc4c2m94viizrhlalxh5z86z9jnb";
+        fetchSubmodules = true;
+      };
+      compiler-nix-name = ghc;
+    }).haskell-language-server.components.exes.haskell-language-server
+
+    (pkgs.haskell-nix.cabalProject {
+      src = sources.hindent;
+      compiler-nix-name = ghc;
+    }).hindent.components.exes.hindent
+
     pkgs."${node}"
-    (import sources.ormolu {}).ormolu
   ];
 
   exactDeps = true;
