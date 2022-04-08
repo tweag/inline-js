842faa9ccb68d9862dcabac9cc44502f89cb7b5f
Cheng Shao <astrohavoc@gmail.com>
2022-04-08 07:33:23 +0000

    Add NamedPipe

---
+++ inline-js-core/src/Language/JavaScript/Inline/Core/NamedPipe/Posix.hs
@@ -0,0 +1,23 @@
+module Language.JavaScript.Inline.Core.NamedPipe.Posix where
+
+import Control.Monad
+import Foreign
+import Foreign.C
+import System.Directory
+import System.IO
+
+mkNamedPipe :: Bool -> IO (String, IO Handle, IO ())
+mkNamedPipe pipe_inbound = do
+  pipe_name <- allocaBytes 256 $ \buf_pipe_name -> do
+    r <- c_mkNamedPipe buf_pipe_name 256
+    unless (toBool r) $ fail "mkNamedPipe failed"
+    peekCString buf_pipe_name
+  pure
+    ( pipe_name,
+      openBinaryFile pipe_name $ if pipe_inbound then ReadMode else WriteMode,
+      removePathForcibly pipe_name
+    )
+
+foreign import ccall unsafe "mkNamedPipe"
+  c_mkNamedPipe ::
+    CString -> CSize -> IO CBool
---
+++ inline-js-core/src/Language/JavaScript/Inline/Core/NamedPipe/Win32.hs
@@ -0,0 +1,28 @@
+module Language.JavaScript.Inline.Core.NamedPipe.Win32 where
+
+import Control.Monad
+import Foreign
+import Foreign.C
+import System.IO
+import System.Win32
+
+mkNamedPipe :: Bool -> IO (String, IO Handle, IO ())
+mkNamedPipe pipe_inbound = do
+  (pipe_name, pipe_handle) <- allocaBytes 256 $ \buf_pipe_name ->
+    alloca $ \buf_pipe_handle -> do
+      r <-
+        c_mkNamedPipe
+          (fromBool pipe_inbound)
+          buf_pipe_name
+          256
+          buf_pipe_handle
+      unless (toBool r) $ fail "mkNamedPipe failed"
+      pipe_name <- peekCString buf_pipe_name
+      pipe_handle <- peek buf_pipe_handle
+      pure (pipe_name, pipe_handle)
+  h <- hANDLEToHandle pipe_handle
+  pure (pipe_name, pure h, pure ())
+
+foreign import ccall unsafe "mkNamedPipe"
+  c_mkNamedPipe ::
+    CBool -> CString -> CSize -> Ptr HANDLE -> IO CBool
